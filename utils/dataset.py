c8538efbcf4c3c0b4eaa1a935a1605f11584392a
tungedng2710 <tungnguyen99.tn@gmail.com>
2022-07-22 11:50:25 +0700

    add neural network training

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,85 +1,122 @@
 import pandas as pd
 import numpy as np
 import os
-from tqdm import tqdm
 
-import torch
-from torch.utils.data import DataLoader, Dataset
-import sklearn
+from torch.utils.data import DataLoader, Dataset, random_split
 from sklearn.model_selection import train_test_split
 
 
 class AI4VN_AirDataset(Dataset):
     def __init__(self, 
                  root_dir: str = "data-train/",
-                 drop_null: bool = False,
-                 get_input_data: bool = True,
-                 get_output_data: bool = False):
+                 mode: str = "input",
+                 drop_null: bool = False):
         """
         root_dir: path to data-train directory
         drop_null: drop row missing data
-        get_input_data: get csv files in the "input" folder
-        get_output_data: get csv files in the "output" folder
+        mode: "input" or "output"
         """
         assert root_dir is not None
         self.root_dir = root_dir
-        self.get_input_data = get_input_data
-        self.get_output_data = get_output_data
         if self.root_dir[-1] != '/':
             self.root_dir = self.root_dir+'/'
+        self.mode = mode
 
         input_df = []
         output_df = []
         print("Loading raw csv files...")
-        if self.get_input_data:
-            for csv_file in tqdm(os.listdir(self.root_dir+"input/")):
-                input_df.append(pd.read_csv(self.root_dir+"input/"+csv_file))
-            self.merged_input_df = pd.concat(input_df, ignore_index=True, sort=False).iloc[:, 1:]
-            if drop_null:
-                self.merged_input_df = self.merged_input_df.dropna()
-            self.columns = list(self.merged_input_df.columns)
+        for csv_file in os.listdir(self.root_dir+"input/"):
+            input_df.append(pd.read_csv(self.root_dir+"input/"+csv_file))
+        self.merged_input_df = pd.concat(input_df, ignore_index=True, sort=False).iloc[:, 1:]
+        if drop_null:
+            self.merged_input_df = self.merged_input_df.dropna()
+        self.columns = list(self.merged_input_df.columns)
 
-        if self.get_output_data:
-            for csv_file in tqdm(os.listdir(self.root_dir+"output/")):
-                output_df.append(pd.read_csv(self.root_dir+"output/"+csv_file))
-            self.merged_output_df = pd.concat(input_df, ignore_index=True, sort=False).iloc[:, 1:]
-            if drop_null:
-                self.merged_output_df = self.merged_output_df.dropna()
-            self.columns = list(self.merged_output_df.columns)
-    
-    def add_location_info(self, 
-                          dataframe: pd.DataFrame,
-                          location_info_df: pd.DataFrame):
-        if dataframe is None:
-            dataframe = self.merged_input_df
-            location_info_df = pd.read_csv(self.root_dir+"location_input.csv")
-        else:
-            assert len(dataframe) != len(location_info_df)
+        for csv_file in os.listdir(self.root_dir+"output/"):
+            output_df.append(pd.read_csv(self.root_dir+"output/"+csv_file))
+        self.merged_output_df = pd.concat(input_df, ignore_index=True, sort=False).iloc[:, 1:]
+        if drop_null:
+            self.merged_output_df = self.merged_output_df.dropna()
+        self.columns = list(self.merged_output_df.columns)
 
-    def __getitem__(self, index):
-        return self.merged_input_df.iloc[index, :]
-
-    def __len__(self):
-        return len(self.merged_input_df)
-
-    def get_data_loader(self, 
-                        test_size: float = 0.2, 
-                        random_state: int = 42):
+        self.X_in, self.y_in, self.X_out, self.y_out = self.preload()
+        
+    def preload(self):
         feat_cols = self.columns
         feat_cols.remove("PM2.5")
         feat_cols.remove("timestamp")
-        if self.get_input_data:
-            y = self.merged_input_df["PM2.5"].values
-            X = self.merged_input_df[feat_cols].values
-            X_train, X_test, y_train, y_test = train_test_split(
-                                                    X, y, test_size=test_size, random_state=random_state
-                                                )
-            return X_train, X_test, y_train, y_test
-        if self.get_output_data:
-            y = self.merged_output_df["PM2.5"].values
-            X = self.merged_output_df[feat_cols].values
-            X_train, X_test, y_train, y_test = train_test_split(
-                                                    X, y, test_size=test_size, random_state=random_state
-                                                )
-            return X_train, X_test, y_train, y_test
+        y_out = self.merged_output_df["PM2.5"].values
+        X_out = self.merged_output_df[feat_cols].values
+        y_in = self.merged_input_df["PM2.5"].values
+        X_in = self.merged_input_df[feat_cols].values
+        return X_in, y_in, X_out, y_out
+
+    def __getitem__(self, index):
+        if self.mode == "input":
+            return self.X_in[index], self.y_in[index]
+        else:
+            return self.X_out[index], self.y_out[index]
+
+
+    def __len__(self):
+        if self.mode == "input":
+            return len(self.merged_input_df)
+        else: 
+            return len(self.merged_output_df)
+
+class AI4VN_AirDataLoader:
+    def __init__(self,
+                 dataset: AI4VN_AirDataset = None,
+                 random_seed: int = 42,
+                 dataset_type: str = "input",
+                 test_size: float = 0.2):
+        """
+        dataset: AI4VN_AirDataset \
+        random_seed (int, default: 64): Controls the shuffling applied to the data before applying the split \
+        dataset_type (str, default: input): use data from "input" folder under the data-train \
+        test_size (float, default: 0.2): Should be between 0.0 and 1.0 and represent the proportion \
+                                         of the dataset to include in the test split.
+        """
+        if dataset is None:
+            self.dataset = AI4VN_AirDataset(drop_null=True, mode=dataset_type)
+        else:
+            self.dataset = dataset
+        self.random_seed = random_seed
+        self.dataset_type = dataset_type
+        self.test_size = test_size
     
+    def get_data_loader_sklearn(self, 
+                                test_size: float = 0.2):
+        if self.dataset_type == "output":
+            X = self.dataset.X_out
+            y = self.dataset.y_out
+            X_train, X_test, y_train, y_test = train_test_split(
+                                                    X, y, test_size=test_size, random_state=self.random_seed
+                                                )
+            return X_train, X_test, y_train, y_test
+        else:
+            X = self.dataset.X_in
+            y = self.dataset.y_in
+            X_train, X_test, y_train, y_test = train_test_split(
+                                                    X, y, test_size=test_size, random_state=self.random_seed
+                                                )
+            return X_train, X_test, y_train, y_test
+
+    def get_data_loader_pytorch(self, 
+                                batch_size: int = 128,
+                                num_workers: int = 8):
+        dataset_size = len(self.dataset)
+        test_size = int(dataset_size*self.test_size)
+        train_size = dataset_size - test_size
+        train_set, val_set = random_split(self.dataset, [train_size, test_size])
+        train_loader = DataLoader(dataset=train_set,
+                                  batch_size=batch_size,
+                                  shuffle=True,
+                                  drop_last=True,
+                                  num_workers=num_workers)
+        val_loader = DataLoader(dataset=val_set,
+                                batch_size=batch_size,
+                                shuffle=False,
+                                num_workers=num_workers)
+        return train_loader, val_loader
+        
--- utils/models.py
+++ utils/models.py
@@ -1,10 +1,11 @@
-import sklearn
 from sklearn import ensemble
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import pickle
 import os
 
+torch.manual_seed(42)
 class EnsembleModel:
     def __init__(self,
                  name: str = "gradientboosting"):
@@ -21,9 +22,11 @@ class EnsembleModel:
         if self.name == "gradientboosting":
             print("Creating Gradient Boosting Regressor")
             self.model = self.gradientboosting
+            self.alias = "GB"
         elif self.name == "randomforest":
             print("Creating Random Forest Regressor")
             self.model = self.randomforest
+            self.alias = "RF"
     
     def fit(self, 
             X_train = None, 
@@ -35,12 +38,30 @@ class EnsembleModel:
         print("Training model...")
         self.model.fit(X_train, y_train)
         if save_checkpoint:
-            with open(save_dir+"EnsembleModel.pkl","wb") as f:
+            with open(save_dir+self.alias+"_model.pkl","wb") as f:
                 pickle.dump(self.model,f)
-                print("Trained model has been saved at "+save_dir+"EnsembleModel.pkl")
+                print("Trained model has been saved at "+save_dir+self.alias+"_model.pkl")
         return self.model
 
+class NormalizedLinear(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(NormalizedLinear, self).__init__()
+        self.W = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.W)
+
+    def forward(self, input):
+        x = F.normalize(input)
+        W = F.normalize(self.W)
+        return F.linear(x, W)
 class NeuralNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(self, 
+                 num_input_feat: int = 2):
+        super(NeuralNetwork, self).__init__()
+        linear1 = NormalizedLinear(num_input_feat, 64)
+        relu = nn.ReLU()
+        linear2 = NormalizedLinear(64, 32)
+        linear3 = NormalizedLinear(32, 1)
+        self.mlp = nn.Sequential(linear1, relu, linear2, relu, linear3)
+    
+    def forward(self, x):
+        return self.mlp(x)
