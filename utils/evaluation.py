3457d40e9333d307ba516f3bc991b6d06c843b2f
quang <thanvanquang190493@gmail.com>
2022-07-25 22:27:15 +0700

    reformat code

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -1,26 +1,36 @@
 import numpy as np
 import sklearn.metrics as metrics
 
-def mean_absolute_percentage_error(y_true, y_pred): 
+
+def mean_absolute_percentage_error(y_true, y_pred):
     y_true, y_pred = np.array(y_true), np.array(y_pred)
     return np.mean(np.abs((y_true + 1 - y_pred) / (y_true + 1)) * 100)
+
 
 def eval_regression_model(model, X_test, y_test):
     y_true = y_test
     y_pred = model.predict(X_test)
 
     # Regression metrics
-    explained_variance=metrics.explained_variance_score(y_true, y_pred)
-    mean_absolute_error=metrics.mean_absolute_error(y_true, y_pred) 
-    mse=metrics.mean_squared_error(y_true, y_pred) 
-    mean_squared_log_error=metrics.mean_squared_log_error(y_true, y_pred)
-    median_absolute_error=metrics.median_absolute_error(y_true, y_pred)
-    r2=metrics.r2_score(y_true, y_pred)
+    explained_variance = metrics.explained_variance_score(y_true, y_pred)
+    mean_absolute_error = metrics.mean_absolute_error(y_true, y_pred)
+    mse = metrics.mean_squared_error(y_true, y_pred)
+    mean_squared_log_error = metrics.mean_squared_log_error(y_true, y_pred)
+    metrics.median_absolute_error(y_true, y_pred)
+    r2 = metrics.r2_score(y_true, y_pred)
 
-    print('explained_variance: ', round(explained_variance,4))    
-    print('mean_squared_log_error: ', round(mean_squared_log_error,4))
-    print('R^2: ', r2)
-    print('MAE: ', mean_absolute_error)
-    print('MSE: ', mse)
-    print('RMSE: ', np.sqrt(mse))
-    print('MAPE: ', mean_absolute_percentage_error(y_true, y_pred), '%')
+    # print('explained_variance: ', round(explained_variance, 4))
+    # print('mean_squared_log_error: ', round(mean_squared_log_error, 4))
+    # print('R^2: ', r2)
+    # print('MAE: ', mean_absolute_error)
+    # print('MSE: ', mse)
+    # print('RMSE: ', np.sqrt(mse))
+    # print('MAPE: ', mean_absolute_percentage_error(y_true, y_pred), '%')
+
+    with open("./metrics.txt", "w") as outfile:
+        outfile.write("explained_variance: " + str(round(explained_variance, 4)) + "\n")
+        outfile.write("R^2: " + str(r2) + "\n")
+        outfile.write("MAE: " + str(mean_absolute_error) + "\n")
+        outfile.write("MSE: " + str(mse) + "\n")
+        outfile.write("RMSE: " + str(np.sqrt(mse)) + "\n")
+        outfile.write("MAPE: " + str(mean_absolute_percentage_error(y_true, y_pred)) + '%' + "\n")
--- utils/models.py
+++ utils/models.py
@@ -6,17 +6,19 @@ import pickle
 import os
 
 torch.manual_seed(42)
+
+
 class EnsembleModel:
     def __init__(self,
                  name: str = "gradientboosting"):
         self.name = name
         gbparams = {
-                    "n_estimators": 500,
-                    "max_depth": 4,
-                    "min_samples_split": 5,
-                    "learning_rate": 0.01,
-                    "loss": "squared_error",
-                   } 
+            "n_estimators": 500,
+            "max_depth": 4,
+            "min_samples_split": 5,
+            "learning_rate": 0.01,
+            "loss": 'ls',
+        }
         self.gradientboosting = ensemble.GradientBoostingRegressor(**gbparams)
         self.randomforest = ensemble.RandomForestRegressor(max_depth=2, random_state=0)
         if self.name == "gradientboosting":
@@ -27,21 +29,22 @@ class EnsembleModel:
             print("Creating Random Forest Regressor")
             self.model = self.randomforest
             self.alias = "RF"
-    
-    def fit(self, 
-            X_train = None, 
-            y_train = None, 
-            save_checkpoint = True,
-            save_dir = "trained_models/"):
+
+    def fit(self,
+            X_train=None,
+            y_train=None,
+            save_checkpoint=True,
+            save_dir="trained_models/"):
         if not os.path.exists(save_dir):
             os.mkdir(save_dir)
         print("Training model...")
         self.model.fit(X_train, y_train)
         if save_checkpoint:
-            with open(save_dir+self.alias+"_model.pkl","wb") as f:
-                pickle.dump(self.model,f)
-                print("Trained model has been saved at "+save_dir+self.alias+"_model.pkl")
+            with open(save_dir + self.alias + "_model.pkl", "wb") as f:
+                pickle.dump(self.model, f)
+                print("Trained model has been saved at " + save_dir + self.alias + "_model.pkl")
         return self.model
+
 
 class NormalizedLinear(nn.Module):
     def __init__(self, in_features, out_features):
@@ -53,8 +56,10 @@ class NormalizedLinear(nn.Module):
         x = F.normalize(input)
         W = F.normalize(self.W)
         return F.linear(x, W)
+
+
 class NeuralNetwork(nn.Module):
-    def __init__(self, 
+    def __init__(self,
                  num_input_feat: int = 2):
         super(NeuralNetwork, self).__init__()
         linear1 = NormalizedLinear(num_input_feat, 64)
@@ -62,6 +67,6 @@ class NeuralNetwork(nn.Module):
         linear2 = NormalizedLinear(64, 32)
         linear3 = NormalizedLinear(32, 1)
         self.mlp = nn.Sequential(linear1, relu, linear2, relu, linear3)
-    
+
     def forward(self, x):
         return self.mlp(x)
